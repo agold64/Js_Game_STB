7ad71879be84f65135d812a018615cde17d4d051
agold64 <andrew.goldstein64@gmail.com>
2015-03-24 12:08:23 -0400

    Refactor utils and html

---
+++ js/functions2.js
@@ -0,0 +1,108 @@
+﻿(function () {
+
+    // Arrays
+    var dice = [];
+    var tile = [];
+
+    // Score
+    var startScore = '45';
+    U.setText('score', startScore);
+
+
+    function fliptile(e) {
+        'use strict';
+        if (typeof e == 'undefined') e = window.event;
+        var target = e.target || e.srcElement;
+    }
+
+    //Flip Tile
+    var button = document.getElementsByTagName('input');
+    for (var i = 0, buttonCount = button.length; i < buttonCount; i++) {
+        var data = button[i];
+
+        data.onclick = function () {
+            tile.push(parseInt(this.value));
+            var avg = tile.reduce(addArray, 0);
+
+            if (dice !== undefined || dice.length !== 0) {
+                if (avg <= dice) {
+                    U.addClass(this.value, 'flipped');
+                    if (avg == dice) {
+                        document.getElementById('rollDice').disabled = false;
+                        startScore -= avg;
+                        document.getElementById('score').textContent = startScore;
+                    }
+                } else {
+                    U.openModal("Number is Higher then Dice");
+                    tile.pop();
+                    avg -= this.value;
+                }
+
+                console.log('Tile ' + tile);
+                console.log('Avg ' + avg);
+                console.log('Score ' + startScore);
+            } else {
+                U.openModal("Roll Dice First");
+            }
+        }
+    }
+
+
+    // Roll Dice
+    function rollDice() {
+        clearArray(dice);
+        clearArray(tile);
+
+        var dice1 = document.getElementById('dice1');
+        var dice2 = document.getElementById('dice2');
+
+        var d1 = Math.floor(Math.random() * 6) + 1;
+        var d2 = Math.floor(Math.random() * 6) + 1;
+
+        var diceTotal = d1 + d2;
+        dice.push(diceTotal);
+
+        dice1.innerHTML = d1;
+        dice2.innerHTML = d2;
+
+        document.getElementById('rollDice').disabled = true;
+        console.log('Rolled ' + dice);
+    }
+
+
+    // Clear Array
+    function clearArray(arr) {
+        while (arr.length) {
+            arr.pop();
+        }
+        //arr.length = 0;
+    }
+
+    // Add Array
+    function addArray(a, b) {
+        return a + b;
+    }
+
+    // End Turn
+    function endTurn() {
+        document.getElementById('rollDice').disabled = true;
+        for (i = 1; i < 10; i++) {
+            U.removeClass([i], 'flipped');
+        }
+        alert("Your final score is " + startScore);
+    }
+
+    // End Game
+    function endGame() {
+        location.reload();
+    }
+
+    // EventHandlers
+    window.onload = function () {
+        U.addEvent(document.getElementById('rollDice'), 'click', rollDice);
+        U.addEvent(document.getElementById('endGame'), 'click', endGame);
+        U.addEvent(document.getElementById('endTurn'), 'click', endTurn);
+        U.addEvent(document.getElementById('closeModal'), 'click', U.closeModal);
+    }
+
+}());
