ad0ff6f8bd9782f24b3b8c46aca2f985ef3ee340
agold64 <andrew.goldstein64@gmail.com>
2015-07-10 02:29:42 -0400

    Refactor functions.js 0.2

--- js/functions.js
+++ js/functions.js
@@ -40,13 +40,12 @@
     function rollDice() {
         clearArray(tile);
 
-        var dice1 = document.getElementById('dice1');
-        var dice2 = document.getElementById('dice2');
+        var dice1 = document.getElementById('dice1'),
+            dice2 = document.getElementById('dice2'),
+            d1 = Math.floor(Math.random() * 6) + 1,
+            d2 = Math.floor(Math.random() * 6) + 1,
+            diceTotal = d1 + d2;
 
-        var d1 = Math.floor(Math.random() * 6) + 1;
-        var d2 = Math.floor(Math.random() * 6) + 1;
-
-        var diceTotal = d1 + d2;
         dice.push(diceTotal);
 
         dice1.innerHTML = d1;
@@ -57,58 +56,50 @@
     }
 
 
-    // Flip Tile
-    function flipTile() {
-        var card = document.getElementsByClassName('card');
-        for (var i = 0, count = card.length; i < count; i++) {
-            var element = document.getElementById(card[i].id);
-            totalTile.push(parseInt(card[i].id));
+    // Flip Function 
+    function flip() {
+        if (dice.length !== 0) {
+            if ((!this.classList.contains('flipped')) && (!this.classList.contains('Locked'))) {
+                tile.push(parseInt(this.id));
+                var avg = tile.reduce(addArray, 0);
 
-            element.onclick = function () {
-                if (dice.length !== 0) {
-                    if ((!this.classList.contains('flipped')) && (!this.classList.contains('Locked'))) {
-                        tile.push(parseInt(this.id));
-                        var avg = tile.reduce(addArray, 0);
-
-                        if (avg <= dice) {
-                            U.addClass(this.id, 'flipped');
-                            if (avg == dice) {
-                                startScore -= avg;
-                                document.getElementById('score').textContent = startScore;
-                                document.getElementById('rollDice').disabled = false;
-                                for (var i in tile) {
-                                    U.addClass(tile[i].toString(), 'Locked');
-                                }
-                                clearArray(dice);
-
-                                for (var i = 0; i < totalTile.length; i++) {
-                                    if (tile.indexOf(totalTile[i]) !== -1) {
-                                        totalTile.splice(i, 1);
-                                    }
-                                }
-                            }
-                        } else {
-                            U.openModal("Number is Higher then Dice");
-                            tile.pop();
+                if (avg <= dice) {
+                    U.addClass(this.id, 'flipped');
+                    if (avg == dice) {
+                        startScore -= avg;
+                        document.getElementById('score').textContent = startScore;
+                        document.getElementById('rollDice').disabled = false;
+                        for (var i in tile) {
+                            U.addClass(tile[i].toString(), 'Locked');
                         }
-                    } else if ((this.classList.contains('flipped')) && (!this.classList.contains('Locked'))) {
-                        U.removeClass(this.id, 'flipped');
-                        removeItem(tile, this.id);
+                        clearArray(dice);
+
+                        for (var i = 0; i < totalTile.length; i++) {
+                            if (tile.indexOf(totalTile[i]) !== -1) {
+                                totalTile.splice(i, 1);
+                            }
+                        }
                     }
                 } else {
-                    U.openModal("Roll Dice");
+                    U.openModal("Number is Higher then Dice");
+                    tile.pop();
                 }
-                console.log('Tile ' + tile);
-                console.log('Total Tile ' + totalTile);
+            } else if ((this.classList.contains('flipped')) && (!this.classList.contains('Locked'))) {
+                U.removeClass(this.id, 'flipped');
+                removeItem(tile, this.id);
             }
+        } else {
+            U.openModal("Roll Dice");
         }
-
+        console.log('Clicked Tile ' + tile);
+        console.log('Total Tile ' + totalTile);
     }
+
 
     // End Turn
     function endTurn() {
         document.getElementById('rollDice').disabled = true;
-        for (i = 1; i < 10; i++) {
+        for (i = 0; i < 10; i++) {
             U.removeClass([i], 'flipped');
         }
         alert("Your final score is " + startScore);
@@ -117,6 +108,19 @@
     // Reset Game
     function resetGame() {
         location.reload();
+        console.log('New Game Starting');
+    }
+
+
+    // Flip Tile
+    function flipTile() {
+        var card = document.getElementsByClassName('card');
+        for (var i = 0, count = card.length; i < count; i++) {
+            var data = card[i].id,
+                element = document.getElementById(data);
+            totalTile.push(parseInt(data));
+            U.addEvent(element, 'click', flip);
+        }
     }
 
     // EventHandlers
